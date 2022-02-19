d6e81df5dffacb93c4ebea3f8dfa01ba5c49705d
Okechukwu Chukwude <chukwudeokechukwu@gmail.com>
2022-02-19 15:28:02 +0100

    Change frontend to React

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import './Home.css'
+import Result from './result'
 
 function Home() {
     const [username, setUsername] = useState('')
     const [disable, setDisable] = useState(false)
-    const [data, setData] = useState('')
+    const [scrappedData, setScrappedData] = useState()
+    const [error, setError] = useState('')
 
     async function scrape (username) {
         try {
@@ -21,25 +23,50 @@ function Home() {
         event.preventDefault()
         setDisable(true)
         const { data } = await scrape(username)
-        console.log(data)
+    
+        if (data === 'An error occured please try again!!' || data === 'Username not found' || data === 'An error occured.\nCheck you network and try again!!') {
+            setError(data)
+        } else {
+            setScrappedData(data)
+        }
         setDisable(false)
     }
 
     return (
-        <>
-            <div className='center'>
-                <span id="message" className="alert alert-danger" role="alert" style={{visibility: "hidden", display: "inline-block"}}></span>
-                <form action="" onSubmit={handleSubmit}>
-                    <h1 className="h3 mb-4 fw-normal" > <i className="bi-github" role="img" aria-label="GitHub"></i> GitHub Scrapper</h1>
-                    <div className="row row-cols d-flex align-items-center">
-                        <div className="input-group mb-3 has-validation">
-                            <span className="input-group-text" id="addon-wrapping">@</span>
-                            <input type="text" className="form-control" id="username" placeholder="GitHub username" required value={username} onChange={e => setUsername(e.target.value)}/>
-                            <button id="btn" className="btn btn-primary" type="submit" disabled={disable}><i className="bi bi-search"></i></button>
-                        </div>              
+        <>   
+            {   scrappedData
+                ?
+                    <div>
+                        <Result scrappedData={scrappedData}/>
                     </div>
-                </form>
-            </div>
+                    
+                :
+                    <div className='center'>
+                        {
+                            error &&
+                                <span id="message" className="alert alert-danger" role="alert" style={{display: "inline-block"}}>{error}</span>
+                        }
+
+                        <form action="" onSubmit={handleSubmit}>
+                            <h1 className="h3 mb-4 fw-normal text-center" > <i className="bi-github" role="img" aria-label="GitHub"></i> GitHub Scrapper</h1>
+                            <div className="row row-cols d-flex align-items-center">
+                                <div className="input-group mb-3 has-validation">
+                                    <span className="input-group-text" id="addon-wrapping">@</span>
+                                    <input type="text" className="form-control" id="username" placeholder="GitHub username" required value={username} onChange={e => setUsername(e.target.value)}/>
+                                    {
+                                        disable
+                                        ?
+                                            <button class="btn btn-primary" type="button" disabled>
+                                                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                            </button>
+                                        :
+                                            <button id="btn" className="btn btn-primary" type="submit"><i className="bi bi-search"></i></button>
+                                    }
+                                </div>              
+                            </div>
+                        </form>
+                    </div>
+            } 
         </>
     )
 }
